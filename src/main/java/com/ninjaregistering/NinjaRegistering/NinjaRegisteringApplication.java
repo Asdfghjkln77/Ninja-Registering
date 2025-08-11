531661aae425d856a5945aa9c5e6aec43fff5886
Asdf <138323469+Asdfghjkln77@users.noreply.github.com>
2025-08-11 20:12:59 -0300

    Add files via upload

---
+++ src/main/java/com/ninjaregistering/NinjaRegistering/NinjaRegisteringApplication.java
@@ -0,0 +1,12 @@
+package com.ninjaregistering.NinjaRegistering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class NinjaRegisteringApplication {
+	public static void main(String[] args) {
+//		SpringApplication.run(NinjaRegisteringApplication.class, args);
+		System.out.println("Ninja Register Application");
+	}
+}
---
+++ src/test/java/com/ninjaregistering/NinjaRegistering/NinjaRegisteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjaregistering.NinjaRegistering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegisteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
